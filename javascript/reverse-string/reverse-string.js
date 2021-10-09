cff161466f0cf8dd476f751aa34358b95a0f0ee2
Katarzyna W <73550714+katarzyna-kw@users.noreply.github.com>
2021-10-09 11:34:09 -0500

    Delete javascript/reverse-string directory

--- javascript/reverse-string/reverse-string.js
+++
@@ -1,6 +0,0 @@
-//
-// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const reverseString = input => input.split('').reverse().join('');
