32df54bbdf874795902e73a19477fcded767d07b
katarzyna_w <katarzyna.koch@gmail.com>
2021-10-24 15:54:06 -0500

    refactor armstrong-numbers

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -3,15 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-export const isArmstrongNumber = (num) => {
+
+ export const isArmstrongNumber = (num) => {
   if (Math.pow(num, num.toString().length)===num) return true
   let numStr=num.toString();
   let sum=0;
   for (let i=0; i<numStr.length; i++) {
     sum+=Math.pow(Number(numStr[i]), numStr.length)
   }
-  if (sum===num) return true
-  else return false
+  return sum===num
 };
-
-console.log(isArmstrongNumber(9926314))
