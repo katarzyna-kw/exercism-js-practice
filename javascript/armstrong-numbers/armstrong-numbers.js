d9c607a74a4d3181e0018d0887a3c7449137af85
katarzyna_w <katarzyna.koch@gmail.com>
2021-10-24 16:28:12 -0500

    refactor armstrong-numbers using reduce

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -4,12 +4,21 @@
 //
 
 
- export const isArmstrongNumber = (num) => {
-  if (Math.pow(num, num.toString().length)===num) return true
-  let numStr=num.toString();
-  let sum=0;
-  for (let i=0; i<numStr.length; i++) {
-    sum+=Math.pow(Number(numStr[i]), numStr.length)
-  }
-  return sum===num
-};
+//  export const isArmstrongNumber = (num) => {
+//   if (Math.pow(num, num.toString().length)===num) return true
+//   let numStr=num.toString();
+//   let sum=0;
+//   for (let i=0; i<numStr.length; i++) {
+//     sum+=Math.pow(Number(numStr[i]), numStr.length)
+//   }
+//   return sum===num
+// };
+
+export const isArmstrongNumber = (num) => {
+  const arr=[...`${num}`]
+  return arr.reduce((prev, current, index, arr) => prev+Math.pow(current, arr.length), 0) === num
+}
+
+
+// console.log(isArmstrongNumber(9926314))
+console.log(isArmstrongNumber(9))
