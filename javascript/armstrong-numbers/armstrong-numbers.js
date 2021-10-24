5f2f8cd88fb92da9e36517b88874ed68cbdf449b
katarzyna_w <katarzyna.koch@gmail.com>
2021-10-24 10:53:58 -0500

    armstrong-numbers solution

---
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -0,0 +1,17 @@
+//
+// This is only a SKELETON file for the 'Armstrong Numbers' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isArmstrongNumber = (num) => {
+  if (Math.pow(num, num.toString().length)===num) return true
+  let numStr=num.toString();
+  let sum=0;
+  for (let i=0; i<numStr.length; i++) {
+    sum+=Math.pow(Number(numStr[i]), numStr.length)
+  }
+  if (sum===num) return true
+  else return false
+};
+
+console.log(isArmstrongNumber(9926314))
